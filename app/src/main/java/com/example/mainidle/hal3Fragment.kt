12244007172d2e630cd62c5ee9ced775370c03af
iqbaalt07 <pudge.warcraft3@gmail.com>
2020-05-14 06:46:28 +0700

    kedelapan v2

--- app/src/main/java/com/example/mainidle/hal3Fragment.kt
+++ app/src/main/java/com/example/mainidle/hal3Fragment.kt
@@ -1,10 +1,12 @@
 package com.example.mainidle
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +19,8 @@ class hal3Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hal3, container, false)
+
     }
+
 
 }
--- app/src/main/java/com/example/mainidle/wolktru.kt
+++ app/src/main/java/com/example/mainidle/wolktru.kt
@@ -1,14 +1,20 @@
 package com.example.mainidle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_wolktru.*
 
 class wolktru : AppCompatActivity() {
-
+    private lateinit var betn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wolktru)
         viewpager.adapter = MyPagerAdapter(supportFragmentManager)
+        betn = findViewById(R.id.button1)
+        betn.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
